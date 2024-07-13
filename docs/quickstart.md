810d35bc00a2f5640f48cb84b5392882003ea947
yanglvyou <yang376607092@gmail.com>
2024-07-13 13:55:59 +0800

    chore: init

---
+++ docs/quickstart.md
@@ -0,0 +1,17 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i galaxy
+
+# 通过 yarn
+yarn add galaxy
+
+# 通过 pnpm
+pnpm add galaxy
+
+# 通过 bun
+bun add galaxy
+```
---
+++ vant.config.mjs
@@ -0,0 +1,39 @@
+export default {
+  name: 'galaxy',
+  build: {
+    css: {
+      preprocessor: 'less',
+    },
+    site: {
+      publicPath: '/galaxy/',
+    },
+  },
+  site: {
+    title: 'galaxy',
+    logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
